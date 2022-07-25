f2a0df755750c2b30a9e099098abfe406a97a2e4
Daniel Lopes <danieljfsantos@gmail.com>
2022-07-25 09:40:38 -0300

    okhttpClient and interceptor

--- PixabayApp/app/src/main/java/br/com/pixabayapp/di/RemoteModule.kt
+++ PixabayApp/app/src/main/java/br/com/pixabayapp/di/RemoteModule.kt
@@ -1,24 +1,55 @@
 package br.com.pixabayapp.di
 
+import br.com.pixabayapp.BuildConfig
 import br.com.pixabayapp.BuildConfig.BASE_URL
 import br.com.pixabayapp.data.source.remote.PixabayApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object RemoteModule {
 
+    private const val TIMEOUT_SECONDS = 15L
+
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            setLevel(
+                if (BuildConfig.DEBUG) {
+                    HttpLoggingInterceptor.Level.BODY
+                } else HttpLoggingInterceptor.Level.NONE
+            )
+        }
+    }
+
+    @Provides
+    fun provideOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .readTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .connectTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .build()
+    }
+
     @Provides
     @Singleton
-    fun providePixabayApi(): PixabayApi {
+    fun providePixabayApi(
+        client: OkHttpClient,
+    ): PixabayApi {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(PixabayApi::class.java)
